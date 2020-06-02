65ea96bb8cd187d3242880ad492fe08e073709e9
arad12panda <aaradhya.bhushan12@gmail.com>
2020-06-03 00:32:12 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon'
+
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule ],
+  declarations: [ AppComponent, TopHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
